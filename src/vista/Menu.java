0499ec823407685a42cafca3c44430bda067bc54
Luis-Silva-Tor <82009772+Luis-Silva-Tor@users.noreply.github.com>
2026-06-27 01:11:39 -0500

    agregar menu

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -1,5 +1,19 @@
 package vista;
 
 public class Menu {
-string test
+// Clase encargada de mostrar el menú principal 
+    public static void mostrar(){
+        System.out.println("\\ gestion de prestamos ");
+        // opciones del sistema 
+        
+        System.out.println("1 Registrar libro");
+        System.out.println("2 Mostrar libros");
+        System.out.println("3 Registrar solicitud");
+        System.out.println("4 Mostrar solicitudes");
+        System.out.println("5 Atender solicitud");
+        System.out.println("6. Reporte");
+        System.out.println("7. Salir");
+        System.out.print("Opcion: ");
+    }
 }
+
