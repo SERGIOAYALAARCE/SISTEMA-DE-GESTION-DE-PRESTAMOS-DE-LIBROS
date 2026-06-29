fd9d07fc7cb26d5d289652d811046a1bdf2a1668
albertolunax15-gif <albertolunax15@gmail.com>
2026-06-28 23:21:41 -0500

    Update GestorBiblioteca.java
    Muestra todos los libros.

--- src/servicio/GestorBiblioteca.java
+++ src/servicio/GestorBiblioteca.java
@@ -7,7 +7,10 @@ import modelo.Solicitud;
 public class GestorBiblioteca {
   private ArbolBinarioBusqueda<Libro> arbol = new ArbolBinarioBusqueda<>();
     private ColaGenerica<Solicitud> cola = new ColaGenerica<>();
+  
     public void registrarLibro(Libro libro){
         arbol.insertar(libro);
         System.out.println("Libro registrado correctamente.");
 }
+      public void mostrarLibros(){
+        arbol.mostrarInOrden();
