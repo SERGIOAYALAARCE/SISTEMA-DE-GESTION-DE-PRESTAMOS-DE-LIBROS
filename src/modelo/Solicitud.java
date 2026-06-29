65a700d5ef626fb837a9ea1a4ad104a68e8ad499
Luis-Silva-Tor <sluismi987@gmail.com>
2026-06-29 02:17:48 -0500

    se crea la clase Solicitud

--- src/modelo/Solicitud.java
+++ src/modelo/Solicitud.java
@@ -1,5 +1,21 @@
 package modelo;
 
+// Clase que representa una solicitud de prestamo
 public class Solicitud {
+
+    private String codigoEstudiante;
+    private String nombreEstudiante;
+    private int codigoLibro;
+    private String fecha;
+
+    // Constructor de la clase
+    public Solicitud(String codigoEstudiante, String nombreEstudiante,
+                     int codigoLibro, String fecha) {
+
+        this.codigoEstudiante = codigoEstudiante;
+        this.nombreEstudiante = nombreEstudiante;
+        this.codigoLibro = codigoLibro;
+        this.fecha = fecha;
+    }
 
 }
