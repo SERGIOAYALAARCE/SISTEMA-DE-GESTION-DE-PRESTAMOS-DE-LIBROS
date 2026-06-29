683d8105ed7654043f909eb10eb8e06ad78a17d7
SERGIOAYALAARCE <142816218+SERGIOAYALAARCE@users.noreply.github.com>
2026-06-28 21:21:45 -0500

    Update Cola.java
    Cola<T> usa una lista enlazada con punteros frente y fin y el método enqueue agrega nodos al final, actualizando punteros e incrementando el tamaño.

--- src/estructuras/Cola.java
+++ src/estructuras/Cola.java
@@ -1,5 +1,20 @@
 package estructuras;
 
 public class Cola {
+      private Nodo<T> frente;
+    private Nodo<T> fin;
+    private int tamanio;
+
+    public void enqueue(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (isEmpty()) {
+            frente = nuevo;
+            fin = nuevo;
+        } else {
+            fin.siguiente = nuevo;
+            fin = nuevo;
+        }
+        tamanio++;
+    }
 
 }
