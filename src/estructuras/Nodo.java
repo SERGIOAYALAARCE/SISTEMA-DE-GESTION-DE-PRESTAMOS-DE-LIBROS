7611ee4454661ce3996de0f9050a515f3d1b7c29
Luis-Silva-Tor <sluismi987@gmail.com>
2026-06-29 02:34:55 -0500

    completo la clase Nodo

--- src/estructuras/Nodo.java
+++ src/estructuras/Nodo.java
@@ -3,13 +3,17 @@ package estructuras;
 // Nodo utilizado por la cola generic
 public class Nodo<T> {
 
+    // Dato que almacena el nodo
+
     T dato;
+    
+    // siguiente nodo
+
     Nodo<T> siguiente;
  // Constructor del nodo 
     public Nodo(T dato) {
         this.dato = dato;
         this.siguiente = null;
     }
-
 
 }
