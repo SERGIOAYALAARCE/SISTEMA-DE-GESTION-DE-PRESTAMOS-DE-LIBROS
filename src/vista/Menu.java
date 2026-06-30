2032e418502a4cae53c36f5a1490c930181c6cc9
Luis-Silva-Tor <sluismi987@gmail.com>
2026-06-30 03:36:22 -0500

    menu Se implementó la ventana principal y el banner de la interfaz grafica

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -37,3 +37,27 @@ public class Menu extends JFrame {
 
         // Layout principal: Division por zonas
         setLayout(new BorderLayout(10, 10));
+    //  ZONA SUPERIOR: Banner con imagen local escalada
+        JLabel etiquetaBanner = new JLabel();
+        etiquetaBanner.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        URL rutaImagen = getClass().getResource("Banner.png");
+        if (rutaImagen != null) {
+            ImageIcon iconoOriginal = new ImageIcon(rutaImagen);
+            
+            Image imagenEscalada = iconoOriginal.getImage().getScaledInstance(1000, 120, Image.SCALE_SMOOTH);
+            etiquetaBanner.setIcon(new ImageIcon(imagenEscalada));
+        } else {
+            // Respaldar con texto plano si el archivo no se encuentra en el paquete
+            etiquetaBanner.setText("QUICKLIBRARY - CATALOGO Y PRESTAMOS");
+            etiquetaBanner.setFont(new Font("Arial", Font.BOLD, 22));
+            etiquetaBanner.setOpaque(true);
+            etiquetaBanner.setBackground(new Color(41, 128, 185));
+            etiquetaBanner.setForeground(Color.WHITE);
+        }
+        
+        // Fijamos la dimensión del contenedor del Banner para proteger el espacio de la pantalla
+        etiquetaBanner.setPreferredSize(new Dimension(1000, 90));
+        add(etiquetaBanner, BorderLayout.NORTH);
+
+       
