9e6c2bedb2c011bbe3c1068b0c6431f67ecab175
SERGIOAYALAARCE <sergio.ayala@ucsm.edu.pe>
2026-06-29 22:53:27 -0500

    Agregar método para insertar elementos en la cola

--- src/estructuras/ColaGenerica.java
+++ src/estructuras/ColaGenerica.java
@@ -4,5 +4,16 @@ public class ColaGenerica {
     private NodoCola<T> frente;
     private NodoCola<T> fin;
     private int tamanio;
+    
+    public void enqueue(T dato){
+        NodoCola<T> nuevo=new NodoCola<>(dato);
 
+        if(isEmpty()){
+            frente=fin=nuevo;
+        }else{
+            fin.siguiente=nuevo;
+            fin=nuevo;
+        }
+        tamanio++;
+    }
 }
